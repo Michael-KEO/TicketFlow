72f887d64f1d5f81e08a051642dac17ff3490c84
micha <keomichael10@gmail.com>
2025-05-28 10:44:54 +0200

    Enhance user form validation by adding email constraints and improving error display for form fields

--- src/Form/UserForm.php
+++ src/Form/UserForm.php
@@ -8,6 +8,9 @@ use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Validator\Constraints\Email;
+use Symfony\Component\Validator\Constraints\NotBlank;
 
 
 class UserForm extends AbstractType
@@ -15,7 +18,12 @@ class UserForm extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')  
+            ->add('email', EmailType::class, [
+                'constraints' => [
+                    new NotBlank(['message' => 'L\'email est obligatoire.']),
+                    new Email(['message' => 'Veuillez entrer un email valide.']),
+                ],
+            ])
             ->add('prenom')
             ->add('nom')
             ->add('plainPassword', PasswordType::class, [
