3c743206f169720b4b9ee6e5a29aa8c9fca3623d
Seynabou NDOYE <seynabou.ndoye@efrei.net>
2025-05-31 12:15:53 +0200

    Modifications par Seynabou

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -4,33 +4,51 @@ namespace App\Controller;
 
 use App\Repository\TicketRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
+use Symfony\Component\Security\Core\User\UserInterface;
 
-#[IsGranted('ROLE_USER')] // Ou un rôle plus spécifique si nécessaire
+#[IsGranted('ROLE_USER')]
 final class DashboardController extends AbstractController
 {
     #[Route('/dashboard', name: 'app_dashboard')]
-    public function index(TicketRepository $ticketRepository): Response
+    public function index(Request $request, TicketRepository $ticketRepository): Response
     {
-        $user = $this->getUser(); // Pour les statistiques spécifiques à l'utilisateur
+        $user = $this->getUser();
+        $activeRole = $request->getSession()->get('active_role');
 
-        // TODO: Appeler les méthodes du TicketRepository pour récupérer les données
-        $ticketsParStatut = $ticketRepository->countTicketsByStatus();
-        $ticketsParPriorite = $ticketRepository->countTicketsByPriority();
-        $mesTicketsEnCours = 0; // Valeur par défaut
-        if ($user instanceof UserInterface) { // S'assurer que $user est bien un UserInterface
-            $mesTicketsEnCours = $ticketRepository->countCurrentUserActiveTickets($user);
+        if (!$activeRole) {
+            return $this->redirectToRoute('homepage');
         }
 
+        return match ($activeRole) {
+            'ROLE_ADMIN' => $this->render('admin/index.html.twig'),
 
-        // TODO: Passer les données au template
-        return $this->render('dashboard/index.html.twig', [
-            'controller_name' => 'DashboardController',
-            'ticketsParStatut' => $ticketsParStatut,
-            'ticketsParPriorite' => $ticketsParPriorite,
-            'mesTicketsEnCours' => $mesTicketsEnCours, // Passer cette nouvelle variable
-        ]);
+            'ROLE_DEV' => $this->render('developer/index.html.twig', [
+                'statistiques' => [
+                    'total' => $ticketRepository->countTicketsByStatusForUser($user, null),
+                    'nouveau' => $ticketRepository->countTicketsByStatusForUser($user, 'Nouveau'),
+                    'assigne' => $ticketRepository->countTicketsByStatusForUser($user, 'Assigné'),
+                    'en_cours' => $ticketRepository->countTicketsByStatusForUser($user, 'En cours'),
+                    'resolu' => $ticketRepository->countTicketsByStatusForUser($user, 'Résolu'),
+                ],
+                'mesTickets' => $ticketRepository->findRecentTicketsForUser($user),
+            ]),
+
+            'ROLE_RAPPORTEUR' => $this->render('reporter/index.html.twig', [
+                'statistiques' => [
+                    'total' => $ticketRepository->countTicketsCreatedByUser($user),
+                    'nouveau' => $ticketRepository->countTicketsByStatusCreatedByUser($user, 'Nouveau'),
+                    'en_cours' => $ticketRepository->countTicketsByStatusCreatedByUser($user, 'En cours'),
+                    'resolu' => $ticketRepository->countTicketsByStatusCreatedByUser($user, 'Résolu'),
+                    'ferme' => $ticketRepository->countTicketsByStatusCreatedByUser($user, 'Fermé'),
+                ],
+                'mesTickets' => $ticketRepository->findRecentTicketsCreatedByUser($user),
+            ]),
+
+            default => $this->redirectToRoute('homepage'),
+        };
     }
 }
--- src/Controller/TicketController.php
+++ src/Controller/TicketController.php
@@ -4,11 +4,12 @@ namespace App\Controller;
 
 use App\Entity\Ticket;
 use App\Entity\Commentaire;
+use App\Entity\User;
 use App\Form\TicketType;
-use App\Form\CommentaireType;
 use App\Form\CommentaireTypeForm;
 use App\Repository\TicketRepository;
 use App\Repository\CommentaireRepository;
+use App\Service\MailNotificationService;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -25,16 +26,12 @@ class TicketController extends AbstractController
     {
         $user = $this->getUser();
         $activeRole = $request->getSession()->get('active_role');
-        
-        // Filtrer les tickets selon le rôle
+
         if ($activeRole === 'ROLE_RAPPORTEUR') {
-            // Le rapporteur voit ses tickets créés
             $tickets = $ticketRepository->findBy(['rapporteur' => $user], ['dateCreation' => 'DESC']);
         } elseif ($activeRole === 'ROLE_DEV') {
-            // Le développeur voit ses tickets assignés
             $tickets = $ticketRepository->findBy(['developpeur' => $user], ['dateCreation' => 'DESC']);
         } else {
-            // L'admin voit tous les tickets
             $tickets = $ticketRepository->findBy([], ['dateCreation' => 'DESC']);
         }
 
@@ -50,20 +47,18 @@ class TicketController extends AbstractController
     {
         $ticket = new Ticket();
         $activeRole = $request->getSession()->get('active_role');
-        
+
         $form = $this->createForm(TicketType::class, $ticket, [
             'is_edit' => false,
             'user_role' => $activeRole
         ]);
-        
+
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            // Générer un ticket_id unique
-            $lastTicket = $entityManager->getRepository(Ticket::class)
-                ->findOneBy([], ['ticket_id' => 'DESC']);
+            $lastTicket = $entityManager->getRepository(Ticket::class)->findOneBy([], ['ticket_id' => 'DESC']);
             $newTicketId = $lastTicket ? $lastTicket->getTicketId() + 1 : 1;
-            
+
             $ticket->setTicketId($newTicketId);
             $ticket->setRapporteur($this->getUser());
             $ticket->setDateCreation(new \DateTime());
@@ -85,28 +80,21 @@ class TicketController extends AbstractController
 
     #[Route('/{id}', name: 'ticket_show', methods: ['GET', 'POST'])]
     #[IsGranted('ROLE_USER')]
-    public function show(
-        Request $request, 
-        Ticket $ticket, 
-        CommentaireRepository $commentaireRepository,
-        EntityManagerInterface $entityManager
-    ): Response {
+    public function show(Request $request, Ticket $ticket, CommentaireRepository $commentaireRepository, EntityManagerInterface $entityManager): Response
+    {
         $user = $this->getUser();
         $activeRole = $request->getSession()->get('active_role');
-        
-        // Vérifier les permissions
+
         if ($activeRole === 'ROLE_RAPPORTEUR' && $ticket->getRapporteur() !== $user) {
             throw $this->createAccessDeniedException('Vous ne pouvez voir que vos propres tickets.');
         }
-        
+
         if ($activeRole === 'ROLE_DEV' && $ticket->getDeveloppeur() !== $user) {
             throw $this->createAccessDeniedException('Vous ne pouvez voir que les tickets qui vous sont assignés.');
         }
 
-        // Récupérer les commentaires
         $commentaires = $commentaireRepository->findByTicketOrderedByDate($ticket->getId());
 
-        // Formulaire pour ajouter un commentaire
         $commentaire = new Commentaire();
         $commentForm = $this->createForm(CommentaireTypeForm::class, $commentaire);
         $commentForm->handleRequest($request);
@@ -114,12 +102,12 @@ class TicketController extends AbstractController
         if ($commentForm->isSubmitted() && $commentForm->isValid()) {
             $commentaire->setTicket($ticket);
             $commentaire->setAuteur($user);
-            
+
             $entityManager->persist($commentaire);
             $entityManager->flush();
 
             $this->addFlash('success', 'Commentaire ajouté avec succès.');
-            
+
             return $this->redirectToRoute('ticket_show', ['id' => $ticket->getId()]);
         }
 
@@ -133,16 +121,15 @@ class TicketController extends AbstractController
 
     #[Route('/{id}/edit', name: 'ticket_edit', methods: ['GET', 'POST'])]
     #[IsGranted('ROLE_USER')]
-    public function edit(Request $request, Ticket $ticket, EntityManagerInterface $entityManager): Response
+    public function edit(Request $request, Ticket $ticket, EntityManagerInterface $entityManager, MailNotificationService $mailService): Response
     {
         $user = $this->getUser();
         $activeRole = $request->getSession()->get('active_role');
-        
-        // Vérifications des permissions
+
         if ($activeRole === 'ROLE_RAPPORTEUR' && $ticket->getRapporteur() !== $user) {
             throw $this->createAccessDeniedException('Vous ne pouvez modifier que vos propres tickets.');
         }
-        
+
         if ($activeRole === 'ROLE_DEV' && $ticket->getDeveloppeur() !== $user && $activeRole !== 'ROLE_ADMIN') {
             throw $this->createAccessDeniedException('Vous ne pouvez modifier que les tickets qui vous sont assignés.');
         }
@@ -151,19 +138,18 @@ class TicketController extends AbstractController
             'is_edit' => true,
             'user_role' => $activeRole
         ]);
-        
+
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            // Si le statut change vers "Résolu" ou "Fermé", enregistrer la date de résolution
-            if (in_array($ticket->getStatutTicket(), ['Résolu', 'Fermé']) && !$ticket->getDateResolution()) {
+            if (in_array($ticket->getStatutTicket(), ['Résolu', 'Fermé', 'Rejeté', 'En cours', 'Ouvert']) && !$ticket->getDateResolution()) {
                 $ticket->setDateResolution(new \DateTime());
             }
 
             $entityManager->flush();
+            $mailService->sendStatusUpdate($ticket);
 
             $this->addFlash('success', 'Le ticket #' . $ticket->getTicketId() . ' a été mis à jour.');
-
             return $this->redirectToRoute('ticket_show', ['id' => $ticket->getId()]);
         }
 
@@ -177,10 +163,10 @@ class TicketController extends AbstractController
     #[IsGranted('ROLE_ADMIN')]
     public function delete(Request $request, Ticket $ticket, EntityManagerInterface $entityManager): Response
     {
-        if ($this->isCsrfTokenValid('delete'.$ticket->getId(), $request->getPayload()->getString('_token'))) {
+        if ($this->isCsrfTokenValid('delete' . $ticket->getId(), $request->getPayload()->getString('_token'))) {
             $entityManager->remove($ticket);
             $entityManager->flush();
-            
+
             $this->addFlash('success', 'Le ticket #' . $ticket->getTicketId() . ' a été supprimé.');
         }
 
@@ -192,15 +178,15 @@ class TicketController extends AbstractController
     public function assign(Request $request, Ticket $ticket, EntityManagerInterface $entityManager): Response
     {
         $developerId = $request->request->get('developer_id');
-        
+
         if ($developerId) {
             $developer = $entityManager->getRepository(User::class)->find($developerId);
             if ($developer && in_array('ROLE_DEV', $developer->getRoles())) {
                 $ticket->setDeveloppeur($developer);
                 $ticket->setStatutTicket('Assigné');
-                
+
                 $entityManager->flush();
-                
+
                 $this->addFlash('success', 'Ticket assigné à ' . $developer->getPrenom() . ' ' . $developer->getNom());
             }
         }
